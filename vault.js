c1035f2d310c5e58bd6778afe2770cda75886253
Bradley <brad@Bradleys-MacBook-Pro.local>
2018-01-02 18:35:59 -1000

    allPassed

--- vault.js
+++ vault.js
@@ -1,4 +1,27 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function(key,value) {
+	let vault = {};
+
+   
+  let setValue = (key,value)=>{
+			return vault[key] = value;
+  }
+
+	let getValue = (key) => {
+		if(vault.hasOwnProperty(key)){
+			return vault[key];
+		}else{
+			return null;
+		}
+	}
+
+
+  return {
+		setValue:setValue,
+		getValue:getValue
+        
+
+    }
+
+
 };
